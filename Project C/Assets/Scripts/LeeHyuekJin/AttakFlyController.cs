b116616bf575a6f9427a152e64e6edbf5cb441d2
HyeukJin <lytmmr@gmail.com>
2024-02-15 15:33:42 +0900

    에셋-uibackground-사운드

--- Project C/Assets/Scripts/LeeHyuekJin/AttakFlyController.cs
+++ Project C/Assets/Scripts/LeeHyuekJin/AttakFlyController.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 
 public class AttakFlyController : MonoBehaviour
 {
-    AIPath _aiPath;
+    private AIPath _aiPath;
+    private float originalSpeed;
+    private void Start()
+    {
+        _aiPath = GetComponent<AIPath>();
+        originalSpeed = _aiPath.maxSpeed;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-
+            StartCoroutine("CollisionPlayer");
         }
+    }
+
+    IEnumerator CollisionPlayer()
+    {
+        Debug.Log("속도0");
+        _aiPath.maxSpeed = 0;
+        yield return new WaitForSeconds(1);
+        Debug.Log("원래속도");
+        _aiPath.maxSpeed = originalSpeed;
+        yield return null;
     }
 }
---
+++ Project C/Assets/Scripts/LeeHyuekJin/HorfController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorfController : MonoBehaviour
+{
+    private GameObject player;
+    private float shootBulletTime;
+    public GameObject bullet;
+    private float bulletForce = 5f;
+    private float AttakCollTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AttakCollTime = 1.5f;
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yDifference = Mathf.Abs(player.transform.position.y - transform.position.y);
+        float xDifference = Mathf.Abs(player.transform.position.x - transform.position.x);
+        bool noObstacleBetween = !Physics2D.Linecast(transform.position, player.transform.position, LayerMask.GetMask("Obstacle"));
+        if (noObstacleBetween && Time.time - shootBulletTime > AttakCollTime && (Mathf.Approximately(yDifference, -5f) || yDifference < 5f))
+        {
+            ShootBullet();
+            shootBulletTime = Time.time;
+        }
+        else if (noObstacleBetween && Time.time - shootBulletTime > AttakCollTime && (Mathf.Approximately(xDifference, -5f) || xDifference < 5f))
+        {
+            ShootBullet();
+            shootBulletTime = Time.time;
+        }
+    }
+
+    private void ShootBullet()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        GameObject Bullet = Instantiate(bullet, transform.position, Quaternion.identity);
+        Rigidbody2D rightBullet_rb = Bullet.GetComponent<Rigidbody2D>();
+        rightBullet_rb.AddForce(direction * bulletForce, ForceMode2D.Impulse);
+    }
+}
