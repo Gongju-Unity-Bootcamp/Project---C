f4a92b1a30a02d9d169c4cc2e7525ae0091dfbf7
Jinho Lee <bravearc69@gmail.com>
2024-02-19 00:07:29 +0900

    UI - 메인씬 생성

---
+++ Project C/Assets/Scripts/LeeJinHo/Menu/MenuButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Debug.Log("클릭");
+        if (gameObject.name.Equals("StartButton"))
+        {
+            Debug.Log("씬");
+            SceneManager.LoadScene(1);
+        }
+
+        if (gameObject.name.Equals("SettingButton"))
+        {
+            //세팅 메뉴 불러오는 메소드
+        }
+
+        if(gameObject.name.Equals("EndButton"))
+        {
+            //Application.Quit();
+        }
+    }
+}
