a40c8a455a17986cf91e9c6e3757d3b24790ff7a
ShinJinhyeok96 <shinjinhyeok@gmail.com>
2024-02-22 00:52:10 +0900

    아이작 시스템 - 피격 상태 구현

--- Project C/Assets/Scripts/ShinJinHyeok/IsaacController.cs
+++ Project C/Assets/Scripts/ShinJinHyeok/IsaacController.cs
@@ -7,18 +7,26 @@ public class IsaacController : MonoBehaviour
 {
     Transform _head;
     Transform _body;
+    Transform _hit;
+
     Animator _headAnimator;
     Animator _bodyAnimator;
+
+    SpriteRenderer _hitSpriteRenderer;
+
+    public Sprite _hitSprite;
 
     private void Awake()
     {
         _head = transform.Find("Head");
         _body = transform.Find("Body");
+        _hit  = transform.Find("Hit");
 
         _headAnimator = _head.GetComponent<Animator>();
         _bodyAnimator = _body.GetComponent<Animator>();
-    }
 
+        _hitSpriteRenderer = _hit.GetComponent<SpriteRenderer>();
+    }
     void Update()
     {
         _headAnimator.SetBool("Up", Input.GetKey(KeyCode.UpArrow));
@@ -28,5 +36,24 @@ public class IsaacController : MonoBehaviour
 
         _bodyAnimator.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
         _bodyAnimator.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(HitCoroutine());
+        }
+    }
+
+    IEnumerator HitCoroutine()
+    {
+        _head.gameObject.SetActive(false);
+        _body.gameObject.SetActive(false);
+        _hit.gameObject.SetActive(true);
+
+        _hitSpriteRenderer.sprite = _hitSprite;
+        yield return new WaitForSeconds(0.5f);
+
+        _head.gameObject.SetActive(true);
+        _body.gameObject.SetActive(true);
+        _hit.gameObject.SetActive(false);
     }
 }
