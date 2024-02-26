90e848d066fcd82ba6bcab3f38496b396e1a9e04
yongmen20 <yongmen20@gmail.com>
2024-02-26 19:12:41 +0900

    보스 사망버그 수정

--- Project C/Assets/Scripts/NamGiSan/MonstroController.cs
+++ Project C/Assets/Scripts/NamGiSan/MonstroController.cs
@@ -10,7 +10,6 @@ public class MonstroController : MonoBehaviour
     private new Collider2D collider;
     private Animator animaotr;
     private BossHealth bossHp;
-    private IsaacController isaac;
 
     public Transform bulletPoint;
     [SerializeField] private float shockwaveTime = 0.75f;
@@ -23,18 +22,11 @@ public class MonstroController : MonoBehaviour
         collider = GetComponent<Collider2D>();
         animaotr = GetComponent<Animator>();
         bossHp = GetComponent<BossHealth>();
-        isaac = GetComponent<IsaacController>();
     }
-
 
     void Start()
     {
         StartCoroutine(RandomState());
-    }
-
-    private void Update()
-    {
-        CheckHp();
     }
 
     void FixedUpdate()
@@ -44,7 +36,7 @@ public class MonstroController : MonoBehaviour
 
     void Flip()
     {
-        if (player.GetComponent<Transform>().position.x > transform.position.x)
+        if (player.transform.position.x > transform.position.x)
         {
             renderer.flipX = true;
         }
@@ -54,58 +46,37 @@ public class MonstroController : MonoBehaviour
         }
     }
 
-    void CheckHp()
-    {
-        if (bossHp.hp <= 0)
-        {
-            collider.enabled = false;
-            rb.velocity = Vector2.zero;
-            StartCoroutine(RandomState());
-        }
-    }
-
     IEnumerator RandomState()
     {
         yield return null;
 
         int randomAction = Random.Range(0, 3);
 
-        if (player != null && bossHp.hp > 0)
+        switch (randomAction)
         {
-            switch (randomAction)
-            {
-                case 0:
-                    StartCoroutine(Chase());
-                    break;
-                case 1:
-                    StartCoroutine(HighJump());
-                    break;
-                case 2:
-                    StartCoroutine(attackReady());
-                    break;
-            }
-        }
-        else
-        {
-            collider.enabled = false;
-            rb.velocity = Vector2.zero;
+            case 0:
+                StartCoroutine(Chase());
+                break;
+            case 1:
+                StartCoroutine(HighJump());
+                break;
+            case 2:
+                StartCoroutine(attackReady());
+                break;
         }
     }
 
 
     IEnumerator Chase()
     {
-        CheckHp();
-
         animaotr.SetTrigger("Chase");
 
         // 대기동작 시간
         yield return new WaitForSeconds(0.125f);
 
-        CheckHp();
+        collider.enabled = false;
         Vector2 startPos = transform.position;
         Vector2 targetPos = player.transform.position;
-        collider.enabled = false;
 
         float moveTime = 0.5f;
         float elapsedTime = 0f;
@@ -120,7 +91,6 @@ public class MonstroController : MonoBehaviour
         // 공중동작 시간
         yield return new WaitForSeconds(0.5f);
         
-        CheckHp();
         transform.position = targetPos;
         rb.velocity = Vector2.zero;
         collider.enabled = true;
@@ -130,12 +100,10 @@ public class MonstroController : MonoBehaviour
     }
 
     IEnumerator HighJump()
-    {
-        CheckHp();
+    {  
         animaotr.SetTrigger("HighJump");
         yield return new WaitForSeconds(0.5f); // 준비 동작 대기시간
 
-        CheckHp();
         collider.enabled = false;
         rb.velocity = new Vector2(0, 70f);
         yield return new WaitForSeconds(0.25f); // 점프 애니메이션 시간
@@ -168,7 +136,6 @@ public class MonstroController : MonoBehaviour
             }
             transform.position = new Vector2(targetPos.x, targetPos.y); // 완전한 착지
 
-            CheckHp();
             int spawnBullet = Random.Range(30, 35);
 
             if (transform.position.y == targetPos.y)
@@ -226,8 +193,6 @@ public class MonstroController : MonoBehaviour
 
     IEnumerator SpitAttack()
     {
-        CheckHp();
-
         animaotr.SetTrigger("Spit");
         yield return new WaitForSeconds(1.0f);  // 원거리 공격 전 대기 동작
         
@@ -235,14 +200,10 @@ public class MonstroController : MonoBehaviour
 
         while (true)
         {
-            CheckHp();
-
             for (int i = 0; i < spawnBullet; i++)
             {
                 int bulletSpeed = Random.Range(6, 8);
                 GameObject bossBullet = BulletManager.instance.GetBulletPool();
-                //GameObject shockWave = transform.GetChild(1).gameObject;
-                //shockWave.GetComponent<ShockWave>().CallShockWave();
                 bossBullet.transform.position = bulletPoint.position;
                 Rigidbody2D rb = bossBullet.GetComponent<Rigidbody2D>();
                 Vector2 dirVec = (player.transform.position - transform.position).normalized;
