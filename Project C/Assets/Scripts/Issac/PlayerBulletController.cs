1678d39712eb26f3aae87b3dc49b45cb423577cc
ShinJinhyeok96 <shinjinhyeok@gmail.com>
2024-02-27 12:43:35 +0900

    303-아이작-풀링-구현-및-충돌-수정

--- Project C/Assets/Scripts/Issac/PlayerBulletController.cs
+++ Project C/Assets/Scripts/Issac/PlayerBulletController.cs
@@ -1,42 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class PlayerBulletController : MonoBehaviour
 {
-    GameObject _playerBullet;
+    public IObjectPool<GameObject> Pool { get; set; }
 
+    Rigidbody2D _bulletRbody;
+    Animator _bulletAnimator;
     public float attakDamage;
+    bool iscoroutine;
 
     void Start()
     {
-        _playerBullet = this.gameObject;
+        _bulletRbody = GetComponent<Rigidbody2D>();
+        _bulletAnimator = GetComponent<Animator>();
     }
+
+    void Update()
+    {
+        if (!iscoroutine)
+        {
+            Init();
+        }
+    }
+
+    void Init()
+    {
+        StartCoroutine("DestroyBulAnim");
+        iscoroutine = true;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        _bulletRbody.gravityScale = 0;
         Managers.Sound.EffectSoundChange("TearImpacts2");
-
-        StartCoroutine(CollisionBullet(_playerBullet));
 
         if (collision.gameObject.CompareTag("Enemy"))
         {
             EnemyHealth enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
             enemyHealth.TakeDamage(attakDamage);
         }
+        
+        StartCoroutine(OnColBullet());
     }
-    IEnumerator CollisionBullet(GameObject bullet)
+
+    IEnumerator OnColBullet()
     {
-        if (bullet != null)
-        {
-            bullet.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            bullet.GetComponent<Animator>().enabled = true;
+        StopCoroutine("DestroyBulAnim");
 
-            yield return new WaitForSeconds(0.5f);
+        Debug.Log("충돌 파괴");
+        _bulletRbody.velocity = Vector2.zero;
+        _bulletAnimator.SetTrigger("Pop");
 
-            if (bullet != null)
-            {
-                Destroy(bullet);
-            }
-        }
+        yield return new WaitForSeconds(0.5f);
+        iscoroutine = false;
+        Pool.Release(gameObject);
+    }
+
+    IEnumerator DestroyBulAnim()
+    {
+        _bulletRbody.gravityScale = 0;
+        float lifeTime = Managers.PlayerStats.bulletSurviveTime + 0.5f;
+
+        Debug.Log("사거리 코루틴 진입");
+        yield return new WaitForSeconds(lifeTime - 0.6f);
+        _bulletRbody.gravityScale = 5;
+
+        yield return new WaitForSeconds(0.1f);
+
+        Managers.Sound.EffectSoundChange("TearImpacts2");
+
+        _bulletRbody.gravityScale = 0;
+        _bulletRbody.velocity = Vector2.zero;
+        _bulletAnimator.SetTrigger("Pop");
+
+        yield return new WaitForSeconds(0.5f);
+
+        iscoroutine = false;
+        Pool.Release(gameObject);
     }
 }
